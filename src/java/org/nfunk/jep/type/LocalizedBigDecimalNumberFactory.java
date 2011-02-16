689fc55bf7dacd56625d853b5300154c0b2d699a
xicmiah <xicmiah@gmail.com>
2011-02-16 10:38:05 +0300

    Documented LocalizedBigDecimalNumberFactory

--- src/java/org/nfunk/jep/type/LocalizedBigDecimalNumberFactory.java
+++ src/java/org/nfunk/jep/type/LocalizedBigDecimalNumberFactory.java
@@ -7,28 +7,44 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
+/**
+ * BigDecimal factory which supports locale-dependant decimal separators
+ */
 public class LocalizedBigDecimalNumberFactory extends BigDecimalNumberFactory {
 
-	NumberFormat format;
+	private DecimalFormat format;
 
+	/**
+	 * Creates a LocalizedBigDecimalNumberFactory from supplied MathContext and Locale
+	 * @param mathContext math context
+	 * @param locale locale
+	 */
 	public LocalizedBigDecimalNumberFactory(MathContext mathContext, Locale locale) {
 		super(mathContext);
-		format = NumberFormat.getNumberInstance(locale);
-		if (format instanceof DecimalFormat) {
-			((DecimalFormat) format).setParseBigDecimal(true);
-		}
+		format = (DecimalFormat) NumberFormat.getNumberInstance(locale);
+		format.setParseBigDecimal(true);
 	}
 
-	public LocalizedBigDecimalNumberFactory(MathContext mathContext, NumberFormat format) {
-		super(mathContext);
-		this.format = format;
+	/**
+	 * Creates a LocalizedBigDecimalNumberFactory from supplied MathContext and default locale
+	 * @param mathContext math context
+	 */
+	public LocalizedBigDecimalNumberFactory(MathContext mathContext) {
+		this(mathContext, Locale.getDefault());
 	}
 
+	/**
+	 * Create BigDecimal instance from supplied string. Uses decimal separators from locale
+	 *
+	 * @param value string to parse
+	 * @return BigDecimal instance from parsed string
+	 */
 	@Override
-	public BigDecimal createNumber(String value) {
+	public BigDecimal createNumber(String value) throws NumberFormatException {
 		try {
 			return (BigDecimal) format.parse(value);
 		} catch (ParseException e) {
+			// Mimic BigDecimal(String) constructor
 			throw new NumberFormatException("Could not parse string " + value);
 		}
 	}
