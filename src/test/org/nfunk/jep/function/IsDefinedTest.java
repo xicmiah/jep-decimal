461fd124aa4f1be0c91bdf6651ebb17f78ff2f65
xicmiah <xicmiah@gmail.com>
2011-05-30 13:42:40 +0400

    Added null support to evaluation

--- src/test/org/nfunk/jep/function/IsDefinedTest.java
+++ src/test/org/nfunk/jep/function/IsDefinedTest.java
@@ -9,6 +9,7 @@ import org.nfunk.jep.VariableFactory;
 import org.nfunk.jep.config.BigDecimalConfig;
 import org.nfunk.jep.config.ConfigurationBuilder;
 import org.nfunk.jep.config.JepConfiguration;
+import org.nfunk.jep.validation.ValidationException;
 
 import java.math.MathContext;
 
@@ -30,18 +31,51 @@ public class IsDefinedTest {
 				.setAllowUndeclaredVariables(true)
 				.createConfig();
 		jep = new JEP(configuration);
+		jep.getEvaluatorVisitor().setTrapNullValues(false);
 	}
 
 	private static Object eval(JEP jep, final String expression) throws ParseException {
 		Node node = jep.parse(expression);
+		jep.validate(node);
 		return jep.evaluate(node);
 	}
 
-	@Test
-	public void testEvaluate() throws Exception {
-		assertEquals(Boolean.FALSE, eval(jep, "isDefined(a)"));
-		assertEquals(Boolean.TRUE, eval(jep, "a = 42; isDefined(a)"));
+	/**
+	 * Test that isDefined is a normal function - undefined variables are rejected
+	 * Depends on scope analysis
+	 * @throws Exception
+	 */
+	@Test(expected = ValidationException.class)
+	public void testUndefinedVariables() throws Exception {
+		eval(jep, "isDefined(a)");
+	}
 
-//		assertEquals(Boolean.FALSE, eval(jep, "a = null; isDefined(a)")); // TODO: define null constant
+	/**
+	 * Test that isDefined returns false if argument is NULL
+	 * @throws Exception
+	 */
+	@Test
+	public void testEvaluation() throws Exception {
+		assertEquals(Boolean.TRUE, eval(jep, "a = 42; isDefined(a)"));
+		assertEquals(Boolean.FALSE, eval(jep, "a = NULL; isDefined(a)"));
+	}
+
+	/**
+	 * Test that isDefined accepts known constants
+	 * @throws Exception
+	 */
+	@Test
+	public void testConstants() throws Exception {
+		assertEquals(Boolean.TRUE, eval(jep, "isDefined(pi)"));
+		assertEquals(Boolean.FALSE, eval(jep, "isDefined(NULL)"));
+	}
+
+	/**
+	 * Test that isDefined falls back to true if argument is not a variable
+	 * @throws Exception
+	 */
+	@Test
+	public void testFallback() throws Exception {
+		assertEquals(Boolean.TRUE, eval(jep, "isDefined(42)"));
 	}
 }
